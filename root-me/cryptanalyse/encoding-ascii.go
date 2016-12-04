ad659cc44a89ce91b9f0d84f364daa4abce13c13
kahlys <youennlaborde@live.fr>
2016-12-05 00:11:28 +0100

    Update encoding-ascii.go

--- root-me/cryptanalyse/encoding-ascii.go
+++ root-me/cryptanalyse/encoding-ascii.go
@@ -8,9 +8,7 @@ import (
 
 func main() {
 	var ip = flag.String("m", "4C6520666C6167206465206365206368616C6C656E6765206573743A203261633337363438316165353436636436383964356239313237356433323465", "hexadecimal string to decode")
-
 	flag.Parse()
-
 	x, err := hex.DecodeString(*ip)
 	if err != nil {
 		panic(err)
